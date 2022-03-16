f3f999162c604468188c9ca8a34dff73c25dcdba
Anreol <12176932+Anreol@users.noreply.github.com>
2022-03-16 13:12:00 +0100

    Initial commit

---
+++ GummyBearMusicDisc.cs
@@ -0,0 +1,70 @@
+﻿using RoR2;
+using UnityEngine;
+
+namespace NovemberThirteenth
+{
+    internal class GummyBearMusicDisc : MonoBehaviour
+    {
+        public int rolledID;
+        public uint playID;
+        public bool hasPaused;
+        public CharacterBody characterBody;
+        public EntityStateMachine bodyStateMachine;
+        public SetStateOnHurt hurtState;
+        public void OnEnable()
+        {
+            if (playID == 0)
+            {
+                rolledID = UnityEngine.Random.Range(0, playEvents.Length);
+                characterBody = gameObject.GetComponent<CharacterBody>();
+                playID = Util.PlaySound(playEvents[rolledID], base.gameObject);
+            }
+            if (bodyStateMachine == null && characterBody)
+            {
+                bodyStateMachine = EntityStateMachine.FindByCustomName(characterBody.gameObject, "Body");
+                hurtState = characterBody.GetComponent<SetStateOnHurt>();
+            }
+        }
+
+        public void FixedUpdate()
+        {
+            bool isStunned = false;
+            bool isShocked = false;
+            if (hurtState)
+            {
+                isStunned = bodyStateMachine.state.GetType() == hurtState.hurtState.stateType;
+                isShocked = bodyStateMachine.state.GetType() == typeof(EntityStates.ShockState);
+            }
+            if ((characterBody.healthComponent.isInFrozenState || isShocked || isStunned ) && !hasPaused)
+            {
+                Util.PlaySound(pauseEvents[rolledID], base.gameObject);
+                hasPaused = true;
+            }
+            if (!characterBody.healthComponent.isInFrozenState && !isShocked && !isStunned && hasPaused)
+            {
+                Util.PlaySound(resumeEvents[rolledID], base.gameObject);
+                hasPaused = false;
+            }
+        }
+        public void OnDisable()
+        {
+            AkSoundEngine.StopPlayingID(playID);
+            playID = 0;
+        }
+        public static string[] playEvents = new string[]
+        {
+            "Play_vat19_gummy",
+            "Play_november_13th"
+        };
+        public static string[] pauseEvents = new string[]
+        {
+            "Pause_vat19_gummy",
+            "Pause_november_13th"
+        };
+        public static string[] resumeEvents = new string[]
+        {
+            "Resume_vat19_gummy",
+            "Resume_november_13th"
+        };
+    }
+}
---
+++ NovemberUnityPlugin.cs
@@ -0,0 +1,45 @@
+﻿using BepInEx;
+using System;
+using System.IO;
+
+namespace NovemberThirteenth
+{
+    [BepInPlugin(ModGuid, ModIdentifier, ModVer)]
+    public class NovemberUnityPlugin : BaseUnityPlugin
+    {
+        internal const string ModVer =
+#if DEBUG
+            "9999." +
+#endif
+            "0.0.1";
+
+        internal const string ModIdentifier = "NovemberThirteenth";
+        internal const string ModGuid = "com.Anreol." + ModIdentifier;
+
+        public static NovemberUnityPlugin instance;
+        public static PluginInfo pluginInfo;
+        public static string soundBankDirectory => System.IO.Path.Combine(Path.GetDirectoryName(pluginInfo.Location), "soundbanks");
+        public void Awake()
+        {
+            pluginInfo = Info;
+            RoR2.CharacterBody.onBodyStartGlobal += Gummyfy;
+        }
+
+        private void Gummyfy(RoR2.CharacterBody obj)
+        {
+            if (obj.inventory.GetItemCount(RoR2.DLC1Content.Items.GummyCloneIdentifier.itemIndex) > 0)
+            {
+                obj.gameObject.AddComponent<GummyBearMusicDisc>();
+            }
+        }
+
+        [RoR2.SystemInitializer]
+        public static void Init()
+        {
+            uint akBankID;
+            AkSoundEngine.AddBasePath(soundBankDirectory);
+            AkSoundEngine.LoadBank("GummyInit.bnk", out akBankID);
+            AkSoundEngine.LoadBank("GummyBank.bnk", out akBankID);
+        }
+    }
+}
